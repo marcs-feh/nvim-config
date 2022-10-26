ab173fd57d290b6f10a6fa7248a1a308a57247d4
marcs-feh <marcosfehlauer@protonmail.com>
2022-10-26 17:35:51 -0300

    add telescope, update cmp.lua to remove deprecated function

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -32,7 +32,6 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
   use "wbthomason/packer.nvim"  -- Have packer manage itself
   use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"   -- Useful lua functions used by lots of plugins
@@ -55,6 +54,11 @@ return packer.startup(function(use)
   -- LSP Support
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
+
+	-- Telescope
+	use {'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	     requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
   -- Automatically set up your config after cloning packer.nvim
   -- Put this at the end after all plugins
