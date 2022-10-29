cee032a1bea4fa225289d1541f23a8d08594e210
marcs-feh <marcosfehlauer@protonmail.com>
2022-10-29 18:31:35 -0300

    add base16 colors

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -79,7 +79,13 @@ opt.shortmess:append "c"
 vim.g.python_recommended_style = 0
 
 -- Colorsheme
-vim.cmd [[color codedark]]
+local colors = require 'conf.base16-colors'
+colors.setup({
+	base00 = '#16161D', base01 = '#2c313c', base02 = '#3e4451', base03 = '#6c7891',
+	base04 = '#565c64', base05 = '#abb2bf', base06 = '#9a9bb3', base07 = '#c5c8e6',
+	base08 = '#e06c75', base09 = '#d19a66', base0A = '#e5c07b', base0B = '#98c379',
+	base0C = '#56b6c2', base0D = '#0184bc', base0E = '#c678dd', base0F = '#a06949',
+})
 
 -- Stop making lines comments when pressing o, this abomination is required
 -- because vim's ftplugins are fucking retarded.
