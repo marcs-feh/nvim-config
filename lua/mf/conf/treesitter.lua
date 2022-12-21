29a8e1422405122cb595f053898e88b3f029e8de
marcs-feh <marcosfehlauer@protonmail.com>
2022-12-21 13:40:44 -0300

    minor tweaks

--- lua/mf/conf/treesitter.lua
+++ lua/mf/conf/treesitter.lua
@@ -5,14 +5,15 @@ configs.setup {
 
   ignore_install = {"phpdoc", "v"},
 
-  highlight = { -- enable highlighting
+  highlight = {
     enable = true,
 		additional_vim_regex_highlighting = false
   },
 
   indent = {
-    enable = true, -- default is disabled anyways
+    enable = true,
   }
 }
 
 --ensure_installed = {"c", "cpp", "python", "lua", "go", "rust", "bash", "latex", "scheme", "javascript", "typescript", "html", "css", "scss", "ninja", "make"},
+
