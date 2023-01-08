98c72a465052bc8ab68e71a0fb56b171259abd06
marcs-feh <marcosfehlauer@protonmail.com>
2023-01-08 13:58:25 -0300

    improve quick align

--- lua/mf/extra/quick-align.lua
+++ lua/mf/extra/quick-align.lua
@@ -7,9 +7,12 @@ QuickAlign = function(buf_num, pattern)
 	local from = vim.api.nvim_buf_get_mark(buf_num, "<")[1]
 	local to = vim.api.nvim_buf_get_mark(buf_num, ">")[1]
 
+	vim.cmd("'<,'>s/\\s*" .. pattern .. "\\s*/ ".. pattern .." /g")
+	vim.cmd [[nohlsearch]] -- clear search
 	local lines = vim.api.nvim_buf_get_lines(buf_num, from - 1, to, true)
 	local new_lines = MiniAlign.align_strings(lines,
 		{ split_pattern = pattern, justify_side = 'left' })
 
 	vim.api.nvim_buf_set_lines(0, from - 1, to, true, new_lines)
 end
+
